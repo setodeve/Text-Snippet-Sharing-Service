437696b8deb32dff9d068e8e05d0c45806d3e568
setodeve <keinick11@outlook.com>
2024-02-16 23:21:46 +0900

    add helper validation function

--- Helpers/ValidationHelper.php
+++ Helpers/ValidationHelper.php
@@ -10,4 +10,16 @@ class ValidationHelper
         if ($value === false) throw new \InvalidArgumentException("The provided value is not a valid integer.");
         return $value;
     }
+
+    public static function stringObject($value): array
+    {
+        foreach ($value as $v) {
+            $sanitized_input = filter_var($v, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            if ($sanitized_input === false){
+                throw new \InvalidArgumentException("The provided value is not a valid string.");
+                exit;
+            }
+        }
+        return $value;
+    }
 }
